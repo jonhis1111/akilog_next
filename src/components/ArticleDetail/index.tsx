1e058f0bd5d74cfd0363d7c357498d5d6c8b5374
jonhis1111 <akito_shinjyo@012grp.co.jp>
2023-03-12 14:08:27 +0900

    Fix: 記事詳細ページ作成(途中)

---
+++ src/components/ArticleDetail/index.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import classes from "src/components/ArticleDetail/ArticleDetail.module.css"
+
+interface Post {
+  id: number;
+  slug: string;
+  title: { rendered: string };
+  date: string;
+}
+
+interface Props {
+  posts: Post[];
+}
+
+export function ArticleDetail({ posts }: Props) {
+  return (
+    <div className={classes.wrap}>
+      <div className={classes.listsWrap}>
+        <ul className={classes.lists}>
+          {posts.map((post) => (
+            <li className={classes.list} key={post.id}>
+              <Link href={`/blog/${post.slug}`}>
+                <h2 className={classes.listTitle}>{post.title.rendered}</h2>
+                <time className={classes.time}>{new Date(post.date).toLocaleDateString()}</time>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
