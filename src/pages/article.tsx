ed272bc1035bb3e79fa075170b18047c8c7d32d9
jonhis1111 <akito_shinjyo@012grp.co.jp>
2023-03-13 10:05:25 +0900

    Fix: 検証

--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,63 +1,65 @@
-import Link from 'next/link';
-import { RecentArticles } from '@/components/RecentArticles'
-import { Profile } from '@/components/Profile';
-import { FirstView } from '@/components/FirstView';
-import { PageNavigation } from '@/components/PageNavigation';
-import { ArticleList } from '@/components/ArticleList';
-import classes from "src/components/Top/Top.module.css"
-import { ArticleDetail } from '@/components/ArticleDetail';
+export{}
 
-interface Post {
-  id: number;
-  slug: string;
-  title: { rendered: string };
-  data: string;
-}
+// import Link from 'next/link';
+// import { RecentArticles } from '@/components/RecentArticles'
+// import { Profile } from '@/components/Profile';
+// import { FirstView } from '@/components/FirstView';
+// import { PageNavigation } from '@/components/PageNavigation';
+// import { ArticleList } from '@/components/ArticleList';
+// import classes from "src/components/Top/Top.module.css"
+// import { ArticleDetail } from '@/components/ArticleDetail';
 
-interface Props {
-  posts: Post[];
-}
+// interface Post {
+//   id: number;
+//   slug: string;
+//   title: { rendered: string };
+//   data: string;
+// }
 
-export default function Blog({ posts } : Props) {
-  return (
-    <div className={classes.allWrap}>
-      <div>
-        <FirstView />
-      </div>
+// interface Props {
+//   posts: Post[];
+// }
 
-      <div className={classes.contentWrap}>
-        <div className={classes.leftWrap}>
-          <div className={classes.naviWrap}>
-            <PageNavigation />
-          </div>
-          <div className={classes.mainWrap}>
-          </div>
-            <ArticleDetail posts={posts}/>
-         </div>
+// export default function Blog({ posts } : Props) {
+//   return (
+//     <div className={classes.allWrap}>
+//       <div>
+//         <FirstView />
+//       </div>
 
-        <div className={classes.rightWrap}>
-          <div className={classes.profileWrap}>
-            <Profile />
-          </div>
-          <div className={classes.recentArticlesWrap}>
-            <RecentArticles
-              posts={posts}
-            />
-          </div>
-        </div>
+//       <div className={classes.contentWrap}>
+//         <div className={classes.leftWrap}>
+//           <div className={classes.naviWrap}>
+//             <PageNavigation />
+//           </div>
+//           <div className={classes.mainWrap}>
+//           </div>
+//             <ArticleDetail posts={posts}/>
+//          </div>
 
-      </div>
-    </div>
-  );
-}
+//         <div className={classes.rightWrap}>
+//           <div className={classes.profileWrap}>
+//             <Profile />
+//           </div>
+//           <div className={classes.recentArticlesWrap}>
+//             <RecentArticles
+//               posts={posts}
+//             />
+//           </div>
+//         </div>
 
-export async function getStaticProps() {
-  const res = await fetch('https://late-arita-7120.under.jp/wp-json/wp/v2/posts');
-  const posts = await res.json();
+//       </div>
+//     </div>
+//   );
+// }
 
-  return {
-    props: {
-      posts,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   const res = await fetch('https://late-arita-7120.under.jp/wp-json/wp/v2/posts');
+//   const posts = await res.json();
+
+//   return {
+//     props: {
+//       posts,
+//     },
+//   };
+// }
