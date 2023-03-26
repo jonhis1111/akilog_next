960faa9890ecbb8465c1bb0c4839ca7a2a0820ca
jonhis1111 <akito_shinjyo@012grp.co.jp>
2023-03-26 12:08:15 +0900

    Fix: 記事詳細ページの表示

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,35 @@
+import Head from 'next/head';
+
+export async function getStaticPaths() {
+  const res = await fetch('https://late-arita-7120.under.jp/wp-json/wp/v2/posts');
+  const posts = await res.json();
+  const paths = posts.map((post) => ({
+    params: { slug: post.slug },
+  }));
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const { slug } = params;
+  const res = await fetch(`https://late-arita-7120.under.jp/wp-json/wp/v2/posts?slug=${slug}`);
+  const [post] = await res.json();
+  return {
+    props: {
+      post,
+    },
+  };
+}
+
+export default function Post({ post }) {
+  return (
+    <>
+      <Head>
+        <title>{post.title.rendered}</title>
+      </Head>
+      <div>
+        <h1>{post.title.rendered}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+      </div>
+    </>
+  );
+}
